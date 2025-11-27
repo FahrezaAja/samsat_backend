7a69d4c1856cf354bc34cf5a6073578c60b9620e
FahrezaAja <fahrezaking@gmail.com>
2025-11-27 16:06:52 +0900

    update-onfig

--- config/config.go
+++ config/config.go
@@ -11,7 +11,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "samsat_user@tcp(127.0.0.1:3306)/samsat_db?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root@tcp(127.0.0.1:3306)/samsat_db?charset=utf8mb4&parseTime=True&loc=Local"
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Gagal koneksi database: ", err)
